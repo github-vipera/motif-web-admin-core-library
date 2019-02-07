f2cc957235ef1daed9e24e71f88adff6975a88fd
Marco Bonati <marco.bonati@vipera.com>
2019-02-07 17:57:36 +0100

    Added Theme Designer support

--- projects/motif-web-admin-core/src/lib/services/index.ts
+++ projects/motif-web-admin-core/src/lib/services/index.ts
@@ -1,4 +1,4 @@
 export * from './ServiceCatalogService';
 export * from './WebAdminCommonServicesModule';
 export * from './web-admin-modules-provider.module'
-export * from './ThemeDesigner/WAThemeDesignerModule'
+export * from './ThemeDesigner/index'
