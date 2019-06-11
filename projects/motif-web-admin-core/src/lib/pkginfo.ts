abefdb8bdab9e5190eaa3c5cf05db2e01ae35972
Marco Tormento <marco.tormento@vipera.com>
2019-06-11 19:43:23 +0200

    Bumped version.

--- projects/motif-web-admin-core/src/lib/pkginfo.ts
+++ projects/motif-web-admin-core/src/lib/pkginfo.ts
@@ -1,9 +1,9 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
     /* tslint:disable */
     export const PKGINFO = {
-    "version": "0.1.1",
-    "timestamp": 1560178129079,
-    "date": "2019-06-10T14:48:49.079Z",
+    "version": "0.1.2",
+    "timestamp": 1560274975855,
+    "date": "2019-06-11T17:42:55.855Z",
     "description": "MOTIF Web Admin Console",
     "contributors": [
         "Marco Tormento (marco.tormento@vipera.com)",
